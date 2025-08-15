02feca2b1b3e1f27a8497d3f2d397de43ffc1bb5
あなたのGitHubユーザー名 <GitHub>
2025-08-15 18:51:29 +0900

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ここにbaseオプションを追加
+  base: '/demo-task-app/', 
   plugins: [react()],
   server: {
     port: 3000,
